eae9774e9c222a91b3f20e85252bfc844660b2a0
Ignacio Sekhard <nachostifler@gmail.com>
2024-09-17 02:09:36 -0300

    origen

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// Variables de entorno
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL or Anon Key is missing!');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Crear cliente de Supabase
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
